f5af4b87aa7fabb2606c6a9514c96a6544459367
Namzuchu <97218806+rogerzuchu21022019@users.noreply.github.com>
2022-11-04 00:40:36 +0700

    finish 6 router and UI

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var chartRouter = require('./routes/chart');
+var registerRouter = require('./routes/register');
+var dataTableRouter = require('./routes/data-table');
+var analysticRouter = require("./routes/analystic");
 
 var app = express();
 
@@ -20,6 +25,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/login', loginRouter);
+app.use('/register', registerRouter);
+app.use("/chart", chartRouter);
+app.use("/data-table", dataTableRouter);
+app.use("/analystic", analysticRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
