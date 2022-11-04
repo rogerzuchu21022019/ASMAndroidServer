2787d68fb145568d648f2c41ae2ea90a3a66f5f0
Namzuchu <97218806+rogerzuchu21022019@users.noreply.github.com>
2022-11-04 21:21:57 +0700

    finished up server

--- bin/server.js
+++ bin/server.js
@@ -3,7 +3,7 @@ require(`dotenv`).config();
 // require(`../src/middlewares/RedisConnection`);
 
 const PORT = process.env.PORT || 3001;
-HTTP_PROXY=`http://proxy.server.com:${PORT}`
+
 app.listen(PORT, () => {
   console.log(`App listening on ${PORT}`);
 });
