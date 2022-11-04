85be5d86afed0596850ff3c4042a516e2da5ae1a
Namzuchu <97218806+rogerzuchu21022019@users.noreply.github.com>
2022-11-04 21:02:43 +0700

    fix heroku postbuild

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,6 @@
 const app = require(`../src/app`);
 require(`dotenv`).config();
-require(`../src/middlewares/RedisConnection`);
+// require(`../src/middlewares/RedisConnection`);
 
 const PORT = process.env.PORT || 3001;
 HTTP_PROXY=`http://proxy.server.com:${PORT}`
