216bc41025b6106304b3edffe5ca20a44f54bb73
Namzuchu <97218806+rogerzuchu21022019@users.noreply.github.com>
2022-11-12 01:52:19 +0700

    update

--- bin/server.js
+++ bin/server.js
@@ -1,8 +1,6 @@
-const app = require(`../src/app`);
+const app = require(`../src/app`)
 require(`dotenv`).config();
-
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3001
+app.listen(PORT,() => {
   console.log(`App listening on ${PORT}`);
-});
+})
